8f3b6ef43f86bdb1a71af86daa2d720a1f3de666
Sung Ryul Cho <hahahoho5811@gmail.com>
2025-09-24 17:10:30 +0900

    Study: 변수를 사용한 자기소개 기능 추가

---
+++ Module01/src/main/java/Practice/MyProfile.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class MyProfile {
+    public static void main(String[] args) {
+        System.out.println("이름: 조성률");
+        System.out.println("나이: 31");
+        System.out.println("자바 공부에 대한 각오 한마디: 화이팅!");
+
+    }
+}
---
+++ Module01/src/main/java/Practice/MyProfile2.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class MyProfile2 {
+    public static void main(String[] args) {
+        String name = "조성률";
+        int age = 31;
+        String motto = "화이팅";
+
+        System.out.println("이름: " + name);
+        System.out.println("나이: " + age);
+        System.out.println("자바 공부에 대한 각오 한마디: " + motto);
+    }
+}
