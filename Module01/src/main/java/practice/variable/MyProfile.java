b2b2d8cdc6ae61e709595979bdf1543c8776c3a1
Sung Ryul Cho <hahahoho5811@gmail.com>
2025-09-24 17:33:50 +0900

    Study: Scanner 사용하기

--- Module01/src/main/java/practice/variable/MyProfile.java
+++ Module01/src/main/java/practice/variable/MyProfile.java
@@ -1,4 +1,4 @@
-package Practice;
+package practice.variable;
 
 public class MyProfile {
     public static void main(String[] args) {
--- Module01/src/main/java/practice/variable/MyProfile2.java
+++ Module01/src/main/java/practice/variable/MyProfile2.java
@@ -1,4 +1,4 @@
-package Practice;
+package practice.variable;
 
 public class MyProfile2 {
     public static void main(String[] args) {
