fd965774560fc0546539768ebd988953c30e93a2
Sung Ryul Cho <hahahoho5811@gmail.com>
2025-09-24 17:43:04 +0900

    Study: if-else 조건문,비교연산자, 나머지 연산자 응용을 통해 홀수 짝수 판별하기

--- Module01/src/main/java/practice/conditionalstatement/OddEvenChecker.java
+++ Module01/src/main/java/practice/conditionalstatement/OddEvenChecker.java
@@ -1,4 +1,15 @@
 package practice.conditionalstatement;
-
+import java.util.Scanner;
 public class OddEvenChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("숫자를 하나 입력하세요!");
+        int num = sc.nextInt();
+        if(num % 2 == 0) {
+            System.out.println("입력하신 숫자는 짝수입니다.");
+        } else {
+            System.out.println("입력하신 숫자는 홀수입니다.");
+        }
+        sc.close();
+    }
 }
